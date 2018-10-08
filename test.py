85597330b8673482244424f37be4fac5d1d837e0
WANG_Sinan <11849180@mail.sustc.edu.cn>
2018-10-08 09:44:34 +0800

    add test cases of forward ANN

--- test.py
+++ test.py
@@ -103,16 +103,34 @@ class FANNTest(unittest.TestCase):
         self.assertListEqual(ann.hidden.tolist(), expect_hidden)
 
     def test_evaluate_given7(self):
+        weights_out = '''-42.8  -32.4   0      -5.6  -23.6  -33.6  -33.6   41.6      0      0     0
+                         -75.3  -32.0   43.2  -41.1  -34.5  -34.8  -34.8   39.8  -58.9      0     0
+                         -85.0  -28.1   28.6  -28.0  -28.0  -28.2  -28.2   29.3  -47.6  -41.3     0
+                          59.9   12.9  -13.5   13.0   13.0   13.0   13.0  -13.4      0      0  81.8
+                      '''
+        weights_out = weights_out.replace('\n', ' 0 ')
+        weights = np.fromstring(weights_out, sep=' ').reshape(4, -1)
+        genp7 = ParityNGenerator(7)
         ann = ForwardArtificialNeuralNectwork(7, 7, 1)
-        weights_out = '''-42.8 -32.4 0 -5.6 -23.6 -33.6 -33.6 41.6 0 0 0
-    -75.3 -32.0 43.2 -41.1 -34.5 -34.8 -34.8 39.8 -58.9 0 0
-    -85.0 -28.1 28.6 -28.0 -28.0 -28.2 -28.2 29.3 -47.6 -41.3 0
-    59.9 12.9 -13.5 13.0 13.0 13.0 13.0 -13.4 0 0 81.8'''
-        weights = []
-        for line in weights_out.split('\n'):
-            row = map(float, line.split())
-            weights.append(list(row))
-        weights = np.array(weights)
+        ann.construct(weights)
+        for _, res, vec in genp7.all():
+            result = ann.evaluate(vec)
+            self.assertEqual(result[0]>0.5, not res)    # use not res since the condition is negating in paper
+
+    def test_evaluate_given8(self):
+        weights_out = '''-12.4   25.2   27.7  -29.4  -28.9  -29.7  -25.4  -28.5   27.8      0      0     0
+                         -40.4   19.6   18.9  -18.1  -19.1  -18.5  -17.3  -18.8   20.4  -67.6      0     0
+                         -48.1   16.0   16.1  -15.9  -16.3  -15.8  -15.9  -15.8   16.7  -55.0  -26.7     0
+                          45.7  -10.0  -11.0   10.0    9.9    9.4   10.0    9.6  -11.4    6.8    2.3  76.3
+                      '''
+        weights_out = weights_out.replace('\n', ' 0 ')
+        weights = np.fromstring(weights_out, sep=' ').reshape(4, -1)
+        genp8 = ParityNGenerator(8)
+        ann = ForwardArtificialNeuralNectwork(8, 8, 1)
+        ann.construct(weights)
+        for _, res, vec in genp8.all():
+            result = ann.evaluate(vec)
+            self.assertEqual(result[0]>0.5, not res)
 
 
 
